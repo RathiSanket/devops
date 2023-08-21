a82e0b5f2131c8df10ce8e224d02cb1a445795d8
RathiSanket <sanket.s.rathi@gmail.com>
2023-08-21 17:27:21 +0530

    init repo

---
+++ calculator/calculator.py
@@ -0,0 +1,10 @@
+def add(first_term, second_term):
+    return (first_term+second_term)
+
+
+def sub(first_term, second_term):
+    return (first_term-second_term)
+
+
+def mul(first_term, second_term):
+    return (first_term*second_term)
---
+++ calculator/test_calculator.py
@@ -0,0 +1,21 @@
+"""
+
+Unit test for the calculator library
+
+"""
+
+import calculator
+
+
+class TestCalculator:
+    def test_addition(self):
+        assert 4 == calculator.add(2, 2)
+
+    def test_addition2(self):
+        assert 5 == calculator.add(2, 3)
+
+    def test_sub(self):
+        assert 2 == calculator.sub(4, 2)
+
+    def test_mul(self):
+        assert 100 == calculator.mul(10, 10)
