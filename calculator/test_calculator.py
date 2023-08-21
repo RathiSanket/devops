7f72b409cfbdbd0125ca8b14d613fdcd2533ca7e
RathiSanket <sanket.s.rathi@gmail.com>
2023-08-21 23:39:01 +0530

    test

--- calculator/test_calculator.py
+++ calculator/test_calculator.py
@@ -15,7 +15,7 @@ class TestCalculator:
         assert 5 == calculator.add(2, 3)
 
     def test_sub(self):
-        assert 2 == calculator.sub(4, 2)
+        assert 1 == calculator.sub(3, 2)
 
     def test_mul(self):
         assert 100 == calculator.mul(10, 10)
